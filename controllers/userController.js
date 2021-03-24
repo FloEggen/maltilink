c3f48c5de0b319ac20486e81377263886044b279
h6vaxe <florent.eggenschwiler@vd.ch>
2021-03-24 16:09:28 +0100

    Version avec database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,43 @@
+const { request } = require('../app')
+
 exports.home = function (req, res) {
     res.render('home-guest')
 }
 
-exports.binch1 = function (req, res) {
-    res.render('7640103300787')
+exports.anyBinch = function (req, res) {
+
+    var mysql = require('mysql')
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Password1234",
+        database: "maltilink"
+    });
+
+    getBeerInfos(con, req.params.code_bar_number)
+        .then(function (results) {
+            console.log(results[0])
+            res.render('binch-info-model', { infos: results[0] })
+        })
+        .catch(function (err) {
+            console.log("Promise rejection error: " + err);
+        })
 }
 
-exports.binch2 = function (req, res) {
-    res.render('7640180521044')
-}
-
-exports.binch3 = function (req, res) {
-    res.render('7640184520050')
-}
-
-exports.binch4 = function (req, res) {
-    res.render('7640399390080')
+getBeerInfos = function (con, beerNumber) {
+    return new Promise(function (resolve, reject) {
+        con.query(
+            "SELECT * FROM bar_code_infos WHERE bar_code_number='" + beerNumber + "'",
+            function (err, rows) {
+                if (rows === undefined) {
+                    reject(new Error("Error rows is undefined"));
+                } else {
+                    resolve(rows);
+                }
+            }
+        )
+    }
+    )
 }
 
--- router.js
+++ router.js
@@ -3,9 +3,7 @@ const router = express.Router()
 const userController = require('./controllers/userController.js')
 router.get('/', userController.home)
 
-router.get('/7640103300787', userController.binch1)
-router.get('/7640180521044', userController.binch2)
-router.get('/7640184520050', userController.binch3)
-router.get('/7640399390080', userController.binch4)
+router.get('/binch/:code_bar_number', userController.anyBinch)
+
 
 module.exports = router
