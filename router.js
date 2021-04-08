9e7e814536228584c1a049684b861a1385d1f8f8
h6vaxe <florent.eggenschwiler@vd.ch>
2021-04-08 14:15:21 +0200

    /binch url removed

--- router.js
+++ router.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const userController = require('./controllers/userController.js')
 router.get('/', userController.home)
 
-router.get('/binch/:code_bar_number', userController.anyBinch, userController.display)
+router.get('/:code_bar_number', userController.anyBinch, userController.display)
 
 module.exports = router
